38b0be1f2dbed72b8a86adba887552ff4bd4431d
Nikita Egorov <avelparmen@gmail.com>
2026-04-18 16:45:42 +0300

    chore: Add LICENSE

--- src/main.c
+++ src/main.c
@@ -1,3 +1,17 @@
+// Copyright 2026 | Nikita Egorov
+// This file is part of blkraid.
+
+// blkraid is free software: you can redistribute it and/or modify it under the terms
+// of the GNU General Public License as published by the Free Software Foundation, 
+// either version 2 of the License, or (at your option) any later version.
+
+// blkraid is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+// PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along with blkraid. 
+// If not, see <https://www.gnu.org/licenses/>.
+ 
 #include <linux/fs.h>
 #include <linux/blkdev.h>
 #include <linux/blk-mq.h>
